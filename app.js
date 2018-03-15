43913fce98500e7c424df715cfea4b64e6df06a7
himanshukumawat44 <kumawathimanshu44@gmail.com>
2018-03-15 13:25:37 +0530

    New Folder

--- app.js
+++ app.js
@@ -7,17 +7,20 @@
         $scope.name="";
         $scope.message="";
         $scope.showMessage=function(){
-            var la=$scope.name.split(",");
-            var l=la.length;
-            if(l<=3 && l>0){
-                $scope.message="Enjoy!";
-            }
-            else if(l==0){
-                $scope.message="Please enter data first"
+            if($scope.name==""){
+                $scope.message="Please enter data first";
             }
             else{
-                $scope.message="Too much!";
+                var la=$scope.name.split(",");
+                var l=la.length;
+                if(l<=3 && l>0){
+                    $scope.message="Enjoy!";
+                }
+                else{
+                    $scope.message="Too much!";
+                }
             }
+
         };
     }
 
